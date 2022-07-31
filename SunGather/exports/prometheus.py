c6ff80e571e1746738854977533a6fbd4fc2bb7d
Jude Cooray <ranhiru@gmail.com>
2022-07-31 17:55:15 +1000

    Cleaning up code

--- SunGather/exports/prometheus.py
+++ SunGather/exports/prometheus.py
@@ -31,12 +31,12 @@ class export_prometheus(object):
         elif metric_type == 'Gauge':
             self.metrics_config[metric_name] =  {
                 'metric_type': metric_type,
-                'metric': Gauge(metric_name, metric['description']),
+                'metric': Gauge(metric_name, metric['description'], unit=metric['unit']),
                 'register': metric['register'],
                 'multiple': metric.get('multiple', 1)
             }
-        # else:
-        #     print("a is greater than b")
+        else:
+            logging.warn(f"Unsupported metric type {metric_type}. Ignoring")
 
     def configure(self, config, inverter):
         try:
@@ -50,75 +50,24 @@ class export_prometheus(object):
         return True
 
     def publish(self, inverter):
-        latest_scrape = inverter.latest_scrape
+        try:
+            latest_scrape = inverter.latest_scrape
 
-        for metric_name, config in self.metrics_config.items():
-            if config['metric_type'] == 'Info':
-                publish = {}
+            for metric_name, config in self.metrics_config.items():
+                if config['metric_type'] == 'Info':
+                    publish = {}
 
-                for info in config['publish_info']:
-                    publish[info['key']] = latest_scrape[info['register']]
+                    for info in config['publish_info']:
+                        publish[info['key']] = latest_scrape[info['register']]
 
-                self.metrics_config[metric_name]['metric'].info(publish)
-            elif config['metric_type'] == 'Enum':
-                self.metrics_config[metric_name]['metric'].state(latest_scrape[config['register']])
-            elif config['metric_type'] == 'Gauge':
-                self.metrics_config[metric_name]['metric'].set(latest_scrape[config['register']] * config['multiple'])
+                    self.metrics_config[metric_name]['metric'].info(publish)
+                elif config['metric_type'] == 'Enum':
+                    self.metrics_config[metric_name]['metric'].state(latest_scrape[config['register']])
+                elif config['metric_type'] == 'Gauge':
+                    self.metrics_config[metric_name]['metric'].set(latest_scrape[config['register']] * config['multiple'])
 
-        # try:
-        #     latest_scrape = inverter.latest_scrape
-        #
-        #     self.device_type_code.info({'device_type_code': latest_scrape['device_type_code']})
-        #     self.run_state.state(latest_scrape['run_state'])
-        #     self.daily_power_yields.set(latest_scrape['daily_power_yields'] * 1000)
-        #     self.total_power_yields.set(latest_scrape['total_power_yields'] * 1000)
-        #     self.internal_temperature.set(latest_scrape['internal_temperature'])
-        #     self.phase_a_voltage.set(latest_scrape['phase_a_voltage'])
-        #     self.total_active_power.set(latest_scrape['total_active_power'])
-        #     self.work_state.info({'work_state': latest_scrape['work_state_1']})
-        #     self.meter_power.set(latest_scrape['meter_power'])
-        #
-        #     logging.debug(f"Prometheus-Published")
-        # except Exception as err:
-        #     logging.error(f"Prometheus-Publishing: Error: {err}")
-        #     return False
-        return True
-
-    # def configure(self, config, inverter):
-    #     try:
-    #         self.device_type_code = Info('inverter_device_type_code', 'Device Type Code')
-    #         self.run_state = Enum("inverter_run_state", "Run State", states=["ON", "OFF"])
-    #         self.daily_power_yields = Gauge("inverter_daily_power_yield_watt_hours_total", "Daily Power Yields")
-    #         self.total_power_yields = Gauge("inverter_total_power_yield_watt_hours_total", "Total Power Yields")
-    #         self.internal_temperature = Gauge("inverter_internal_temperature_celsius", "Internal Temperature")
-    #         self.phase_a_voltage = Gauge("inverter_phase_a_voltage_volts", "Phase A Voltage")
-    #         self.total_active_power = Gauge("inverter_active_power_watts_total", "Total Active Power")
-    #         self.work_state = Info('inverter_work_state', 'Work State')
-    #         self.meter_power = Gauge("inverter_meter_power_watts_total", "Meter Power")
-    #
-    #         start_http_server(config.get('port', 8000))
-    #     except Exception as err:
-    #         logging.error(f"Prometheus-Export: Error: {err}")
-    #         return False
-    #     return True
-    #
-    #
-    # def publish(self, inverter):
-    #     try:
-    #         latest_scrape = inverter.latest_scrape
-    #
-    #         self.device_type_code.info({'device_type_code': latest_scrape['device_type_code']})
-    #         self.run_state.state(latest_scrape['run_state'])
-    #         self.daily_power_yields.set(latest_scrape['daily_power_yields'] * 1000)
-    #         self.total_power_yields.set(latest_scrape['total_power_yields'] * 1000)
-    #         self.internal_temperature.set(latest_scrape['internal_temperature'])
-    #         self.phase_a_voltage.set(latest_scrape['phase_a_voltage'])
-    #         self.total_active_power.set(latest_scrape['total_active_power'])
-    #         self.work_state.info({'work_state': latest_scrape['work_state_1']})
-    #         self.meter_power.set(latest_scrape['meter_power'])
-    #
-    #         logging.debug(f"Prometheus-Published")
-    #     except Exception as err:
-    #         logging.error(f"Prometheus-Publishing: Error: {err}")
-    #         return False
-    #     return True
+            return True
+            logging.debug(f"Prometheus-Published")
+        except Exception as err:
+            logging.error(f"Prometheus-Publishing: Error: {err}")
+            return False
